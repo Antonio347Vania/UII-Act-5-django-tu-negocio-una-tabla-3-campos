93e1559b3833c4eaaad13a64b78ec9393e328dd1
Antonio347Vania <a.23308051280469@cbtis128.edu.mx>
2025-10-07 12:26:52 -0600

    Add files via upload

---
+++ UII Act 5 tabla Vania Antonio/app_Productos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+#Clase Productos
+class Productos(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nombre} - ${self.precio}"
